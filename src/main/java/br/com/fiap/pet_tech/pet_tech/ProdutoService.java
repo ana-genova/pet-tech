bff42a0b97af44853fdfeae24e2f917511e216b0
Ana Genova <anagenova9@outlook.com>
2024-10-06 14:10:07 -0300

    Aula 2 - feat: Tratativas de exceção

--- src/main/java/br/com/fiap/pet_tech/pet_tech/ProdutoService.java
+++ src/main/java/br/com/fiap/pet_tech/pet_tech/ProdutoService.java
@@ -1,8 +1,10 @@
 package br.com.fiap.pet_tech.pet_tech;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.naming.ldap.Control;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,8 +20,8 @@ public class ProdutoService {
         return produtos;
     }
 
-    public Optional<Produto> findById(UUID id) {
-        var produto = produtoRepository.findById(id);
+    public Produto findById(UUID id) {
+        var produto = produtoRepository.findById(id).orElseThrow(() -> new ControllerNotFoundException("Produto não encontrado"));
         return produto;
     }
 
@@ -29,14 +31,17 @@ public class ProdutoService {
     }
 
     public Produto update(UUID id, Produto produto) {
-        Produto buscaProduto = produtoRepository.getOne(id);
-        buscaProduto.setNome(produto.getNome());
-        buscaProduto.setDescricao(produto.getDescricao());
-        buscaProduto.setUrlDaImage(produto.getUrlDaImage());
-        buscaProduto.setPreco(produto.getPreco());
-        buscaProduto = produtoRepository.save(buscaProduto);
-
-        return buscaProduto;
+        try {
+            Produto buscaProduto = produtoRepository.getOne(id);
+            buscaProduto.setNome(produto.getNome());
+            buscaProduto.setDescricao(produto.getDescricao());
+            buscaProduto.setUrlDaImage(produto.getUrlDaImage());
+            buscaProduto.setPreco(produto.getPreco());
+            buscaProduto = produtoRepository.save(buscaProduto);
+            return buscaProduto;
+        } catch (EntityNotFoundException e) {
+            throw new ControllerNotFoundException("Produto não encontrado");
+        }
     }
 
     public void delete(UUID id) {
